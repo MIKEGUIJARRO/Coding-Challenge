724a03f72fcfe09689e75b96c5df06a421d17a5d
Mike Guijarro <a01612042@itesm.mx>
2022-04-14 15:33:14 -0400

    Classname changse on card implemented

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,11 +3,11 @@ import React from 'react'
 const Card = ({ title, body }) => {
     return (
         
-            <div class="max-w-sm rounded overflow-hidden shadow-lg max-w-md w-full">
-                <img class="w-full" src='https://picsum.photos/200/100' alt="Sunset in the mountains" />
-                <div class="px-6 py-4">
-                    <div class="font-bold text-xl mb-2">{title}</div>
-                    <p class="text-gray-700 text-base">
+            <div className="max-w-sm rounded overflow-hidden shadow-lg max-w-md w-full">
+                <img className="w-full" src='https://picsum.photos/200/100' alt="Sunset in the mountains" />
+                <div className="px-6 py-4">
+                    <div className="font-bold text-xl mb-2">{title}</div>
+                    <p className="text-gray-700 text-base">
                         {body}
                     </p>
                 </div>
