440c8c125f9935c6a3786b08cc7b537d72078c0e
Mike Guijarro <a01612042@itesm.mx>
2022-04-14 15:31:30 -0400

    Safe access to user implemented

--- src/pages/UserInfo.js
+++ src/pages/UserInfo.js
@@ -18,7 +18,7 @@ const UserInfo = () => {
             <Scaffold>
                 <h1 className='py-12'>Users Info</h1>
                 {userData && <h2 className='pb-12 text-gray-700'>{userData[0]?.name}</h2>}
-                {userData.length === 0 && <h2 className='pb-12 text-gray-700'>Undefined User 👀</h2>}
+                {userData && userData?.length === 0 && <h2 className='pb-12 text-gray-700'>Undefined User 👀</h2>}
                 <div className='flex flex-col flex-row justify-center items-center space-y-8'>
                     {postsIsPending && <h2>Loading...</h2>}
                     {postsError && <h2>{postsError}</h2>}
